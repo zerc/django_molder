db88fdcdcf0072707981e48741bb6aa5e2c66b4c
zero13cool <zero13cool@yandex.ru>
2015-05-19 22:55:01 +0300

    Update default config

--- django_molder/config.py
+++ django_molder/config.py
@@ -5,7 +5,9 @@ from django.conf import settings
 
 MOLDER_DEFAULT_CLASSES_FOR_WIDGETS = \
     getattr(settings, 'MOLDER_DEFAULT_CLASSES_FOR_WIDGETS', {
-        None: '',  # default
         forms.TextInput: 'form-control',
         forms.ClearableFileInput: 'fileinput',
     })
+
+# default class for widgets not listed at `MOLDER_DEFAULT_CLASSES_FOR_WIDGETS`
+MOLDER_DEFAULT_CLASSES_FOR_WIDGETS.setdefault(None, '')
